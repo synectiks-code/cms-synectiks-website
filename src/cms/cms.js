be44adfd4c18deea8f770b9228bac993a47fbfac
Jasmin Makasana <jasminmakasana1991@gmail.com>
2021-04-01 12:23:59 +0530

    Fix: issue with the 404 fixed.

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -2,9 +2,17 @@ import CMS from 'netlify-cms-app'
 import uploadcare from 'netlify-cms-media-library-uploadcare'
 import cloudinary from 'netlify-cms-media-library-cloudinary'
 
-import IndexPagePreview from './preview-templates/IndexPagePreview'
+import IndexPagePreview from './preview-templates/IndexPagePreview';
+import BlogPostPreview from './preview-templates/BlogPostPreview';
+import SurveyTemplate from './preview-templates/SurveyPreview';
+import ScenarioPagePreview from './preview-templates/ScenarioPagePreview';
+import ServicePostPreview from './preview-templates/ServicePostPreview';
 
 CMS.registerMediaLibrary(uploadcare)
 CMS.registerMediaLibrary(cloudinary)
 
-CMS.registerPreviewTemplate('index', IndexPagePreview)
+CMS.registerPreviewTemplate('index', IndexPagePreview);
+CMS.registerPreviewTemplate('blog', BlogPostPreview);
+CMS.registerPreviewTemplate('survey', SurveyTemplate);
+CMS.registerPreviewTemplate('scenario', ScenarioPagePreview);
+CMS.registerPreviewTemplate('service', ServicePostPreview);
