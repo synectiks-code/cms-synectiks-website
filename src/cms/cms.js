388a0c74b4b49c1f66efb9f5bce4fd3025a783e1
mustafa-synectiks <35029729+mustafa-synectiks@users.noreply.github.com>
2021-05-17 17:13:22 +0530

    Case Study Added

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,18 +1,20 @@
-import CMS from 'netlify-cms-app'
-import uploadcare from 'netlify-cms-media-library-uploadcare'
-import cloudinary from 'netlify-cms-media-library-cloudinary'
+import CMS from 'netlify-cms-app';
+import uploadcare from 'netlify-cms-media-library-uploadcare';
+import cloudinary from 'netlify-cms-media-library-cloudinary';
 
 import IndexPagePreview from './preview-templates/IndexPagePreview';
 import BlogPostPreview from './preview-templates/BlogPostPreview';
+import CasePostPreview from './preview-templates/CasePostPreview';
 import SurveyTemplate from './preview-templates/SurveyPreview';
 import ScenarioPagePreview from './preview-templates/ScenarioPagePreview';
 import ServicePagePreview from './preview-templates/ServicePostPreview';
 
-CMS.registerMediaLibrary(uploadcare)
-CMS.registerMediaLibrary(cloudinary)
+CMS.registerMediaLibrary(uploadcare);
+CMS.registerMediaLibrary(cloudinary);
 
 CMS.registerPreviewTemplate('index', IndexPagePreview);
 CMS.registerPreviewTemplate('blog', BlogPostPreview);
+CMS.registerPreviewTemplate('case', CasePostPreview);
 CMS.registerPreviewTemplate('survey', SurveyTemplate);
 CMS.registerPreviewTemplate('scenario', ScenarioPagePreview);
 CMS.registerPreviewTemplate('service', ServicePagePreview);
