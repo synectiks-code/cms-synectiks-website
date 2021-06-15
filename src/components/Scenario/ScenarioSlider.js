3fa8d238c7b492e0b6b35ac3322957429b0f62ce
mustafa-synectiks <35029729+mustafa-synectiks@users.noreply.github.com>
2021-06-15 21:55:14 +0530

    Show and Hide dashboard on select scenario

--- src/components/Scenario/ScenarioSlider.js
+++ src/components/Scenario/ScenarioSlider.js
@@ -1,57 +1,66 @@
-import React, { useState, useRef } from 'react'
-import PropTypes from 'prop-types'
-import { v4 } from 'uuid'
-import { AiOutlineLeft, AiOutlineRight, AiOutlineFullscreen, AiFillCloseCircle } from "react-icons/ai";
-import PhotoswipeUIDefault from 'photoswipe/dist/photoswipe-ui-default'
-import 'photoswipe/dist/photoswipe.css'
-import 'photoswipe/dist/default-skin/default-skin.css'
-import { CustomGallery, Item, DefaultLayout } from 'react-photoswipe-gallery'
+import React, { useState, useRef } from 'react';
+import PropTypes from 'prop-types';
+import { v4 } from 'uuid';
+import {
+  AiOutlineLeft,
+  AiOutlineRight,
+  AiOutlineFullscreen,
+  AiFillCloseCircle,
+} from 'react-icons/ai';
+import PhotoswipeUIDefault from 'photoswipe/dist/photoswipe-ui-default';
+import 'photoswipe/dist/photoswipe.css';
+import 'photoswipe/dist/default-skin/default-skin.css';
+import { CustomGallery, Item, DefaultLayout } from 'react-photoswipe-gallery';
 import { Scrollbars } from 'react-custom-scrollbars';
 
 const ScenarioSlider = ({ slider, showMoreDetailsButton }) => {
   const [currentSlide, setCurrentSlide] = useState(0);
   const [showMoreDetails, setShowMoreDetails] = useState(false);
-  const layoutRef = useRef()
+  const [showDetails, setShowDetails] = useState(false);
+  const layoutRef = useRef();
 
   function gotoNextSlide() {
-    if ((currentSlide + 1) < slider.length) {
+    if (currentSlide + 1 < slider.length) {
       setCurrentSlide(currentSlide + 1);
     }
-  };
+  }
 
   function goToPreviousSlide() {
     if (currentSlide - 1 >= 0) {
       setCurrentSlide(currentSlide - 1);
     }
-  };
+  }
 
   function onClickMoreDetails() {
     setShowMoreDetails(true);
-  };
+  }
 
   function onClickMoreDetailsclose() {
     setShowMoreDetails(false);
-  };
+  }
 
   return (
     <>
       <CustomGallery layoutRef={layoutRef} ui={PhotoswipeUIDefault}>
         {slider.map((sliderContent, index) => (
-          <article key={v4()} className={`slider-content ${currentSlide === index ? 'active' : ''}`}>
+          <article
+            key={v4()}
+            className={`slider-content ${
+              currentSlide === index ? 'active' : ''
+            }`}>
             {/* <Scrollbars> */}
-            <div className="item-image">
+            <div className='item-image'>
               <Item
                 original={sliderContent.img}
                 thumbnail={sliderContent.img}
-                width="1920"
-                height="1280"
-              >
+                width='1920'
+                height='1280'>
                 {({ ref, open }) => (
-                  <div ref={ref} onClick={open} className="item-image-wrapper">
-                    <button className="fullscreen">
+                  <div ref={ref} onClick={open} className='item-image-wrapper'>
+                    <button className='fullscreen'>
                       <AiOutlineFullscreen />
                     </button>
-                    <div className="item-image-div">
+                    <div className='item-image-div'>
                       <img
                         src={sliderContent.img}
                         alt={sliderContent.name}
@@ -62,84 +71,122 @@ const ScenarioSlider = ({ slider, showMoreDetailsButton }) => {
                 )}
               </Item>
             </div>
-            <div className="item-content">
-              <div className="container-fluid">
-                <div className="row">
-                  <div className="col-md-10 col-12">
-                    <h3 className="has-text-weight-semibold">
-                      {sliderContent.name}
-                    </h3>
-                  </div>
-                  {
-                    showMoreDetailsButton &&
-                    <div className="col-md-2 col-12">
-                      <button style={{ marginTop: '7px' }} className="button is-link float-right more-details-btn" onClick={onClickMoreDetails}>More details</button>
-                      {showMoreDetails &&
-                        <div
-                          className="more-details-content"
-                          style={{
-                            backgroundImage: `url(${sliderContent.img})`,
-                          }}
-                        >
-                          <Scrollbars>
-                            <div className="details-content-inner">
-                              <button className="close-btn" onClick={onClickMoreDetailsclose}>
-                                <AiFillCloseCircle />
-                              </button>
-                              <div className="container">
-                                <div className="row">
-                                  <div className="col-md-8 col-12">
-                                    <h3 className="has-text-weight-semibold is-size-2">
-                                      {sliderContent.moreDetails.moreDetailsName}
-                                    </h3>
-                                    <p>
-                                      {sliderContent.moreDetails.moreDetailsText}
-                                    </p>
-                                  </div>
-                                  <div className="col-md-4 col-12">
-                                    {
-                                      sliderContent.moreDetails.moreDetailsImage && sliderContent.moreDetails.moreDetailsImage.map((img, index) =>
-                                        <div key={v4()} className="image">
-                                          <img
-                                            src={img.img}
-                                            alt={sliderContent.moreDetails.moreDetailsName}
-                                            title={sliderContent.moreDetails.moreDetailsName}
-                                            width="300"
-                                            height="200"
-                                          />
-                                        </div>
-                                      )
-                                    }
+            {showDetails ? (
+              <div className='item-content'>
+                <div className='container-fluid'>
+                  <div className='row'>
+                    <div className='col-md-10 col-12'>
+                      <button
+                        className='close-more'
+                        onClick={() => setShowDetails(false)}>
+                        Close
+                      </button>
+                      <h3 className='has-text-weight-semibold'>
+                        {sliderContent.name}
+                        adding here
+                      </h3>
+                    </div>
+                    {showMoreDetailsButton && (
+                      <div className='col-md-2 col-12'>
+                        <button
+                          style={{ marginTop: '7px' }}
+                          className='button is-link float-right more-details-btn'
+                          onClick={onClickMoreDetails}>
+                          More details
+                        </button>
+                        {showMoreDetails && (
+                          <div
+                            className='more-details-content'
+                            style={{
+                              backgroundImage: `url(${sliderContent.img})`,
+                            }}>
+                            <Scrollbars>
+                              <div className='details-content-inner'>
+                                <button
+                                  className='close-btn'
+                                  onClick={onClickMoreDetailsclose}>
+                                  <AiFillCloseCircle />
+                                </button>
+                                <div className='container'>
+                                  <div className='row'>
+                                    <div className='col-md-8 col-12'>
+                                      <h3 className='has-text-weight-semibold is-size-2'>
+                                        {
+                                          sliderContent.moreDetails
+                                            .moreDetailsName
+                                        }
+                                      </h3>
+                                      <p>
+                                        {
+                                          sliderContent.moreDetails
+                                            .moreDetailsText
+                                        }
+                                      </p>
+                                    </div>
+                                    <div className='col-md-4 col-12'>
+                                      {sliderContent.moreDetails
+                                        .moreDetailsImage &&
+                                        sliderContent.moreDetails.moreDetailsImage.map(
+                                          (img, index) => (
+                                            <div key={v4()} className='image'>
+                                              <img
+                                                src={img.img}
+                                                alt={
+                                                  sliderContent.moreDetails
+                                                    .moreDetailsName
+                                                }
+                                                title={
+                                                  sliderContent.moreDetails
+                                                    .moreDetailsName
+                                                }
+                                                width='300'
+                                                height='200'
+                                              />
+                                            </div>
+                                          )
+                                        )}
+                                    </div>
                                   </div>
                                 </div>
                               </div>
-                            </div>
-                          </Scrollbars>
-                        </div>
-                      }
-                    </div>
-                  }
+                            </Scrollbars>
+                          </div>
+                        )}
+                      </div>
+                    )}
+                  </div>
                 </div>
-              </div>
-              <div className="container-fluid">
-                <div className="row">
-                  <div className="col-12">
-                    <p>
-                      {sliderContent.text}
-                    </p>
+                <div className='container-fluid'>
+                  <div className='row'>
+                    <div className='col-12'>
+                      <p>
+                        {sliderContent.text}
+                        adding here Security_and_Compliance_ifdi30
+                      </p>
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
+            ) : null}
+
             {/* </Scrollbars> */}
           </article>
         ))}
-        <ul className="pager">
-          {slider.map((sliderContent, index) => (
-            <li key={v4()} onClick={() => setCurrentSlide(index)} className={`pager-item ${currentSlide === index ? 'active' : ''}`}>
-            </li>
-          ))}
-        </ul>
+        <div>
+          <ul className='pager'>
+            {slider.map((sliderContent, index) => (
+              <li
+                key={v4()}
+                onClick={() => setCurrentSlide(index)}
+                className={`pager-item ${
+                  currentSlide === index ? 'active' : ''
+                }`}></li>
+            ))}
+          </ul>
+          <button className='learn-more' onClick={() => setShowDetails(true)}>
+            Learn More
+          </button>
+        </div>
         <button className='nabtn-left' onClick={goToPreviousSlide}>
           <AiOutlineLeft />
         </button>
@@ -176,6 +223,6 @@ ScenarioSlider.propTypes = {
       ),
     })
   ),
-}
+};
 
-export default ScenarioSlider
+export default ScenarioSlider;
--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,26 +1,30 @@
-import React, { useState, useRef } from 'react'
-import PropTypes from 'prop-types'
-import { v4 } from 'uuid'
-import { AiOutlineLeft, AiOutlineRight, AiOutlineFullscreen } from "react-icons/ai";
-import PhotoswipeUIDefault from 'photoswipe/dist/photoswipe-ui-default'
-import 'photoswipe/dist/photoswipe.css'
-import 'photoswipe/dist/default-skin/default-skin.css'
-import { CustomGallery, Item, DefaultLayout } from 'react-photoswipe-gallery'
+import React, { useState, useRef } from 'react';
+import PropTypes from 'prop-types';
+import { v4 } from 'uuid';
+import {
+  AiOutlineLeft,
+  AiOutlineRight,
+  AiOutlineFullscreen,
+} from 'react-icons/ai';
+import PhotoswipeUIDefault from 'photoswipe/dist/photoswipe-ui-default';
+import 'photoswipe/dist/photoswipe.css';
+import 'photoswipe/dist/default-skin/default-skin.css';
+import { CustomGallery, Item, DefaultLayout } from 'react-photoswipe-gallery';
 
 const Slider = ({ slider }) => {
   const [currentSlide, setCurrentSlide] = useState(0);
-  const layoutRef = useRef()
+  const layoutRef = useRef();
   function gotoNextSlide() {
-    if ((currentSlide + 1) < slider.length) {
+    if (currentSlide + 1 < slider.length) {
       setCurrentSlide(currentSlide + 1);
     }
-  };
+  }
 
   function goToPreviousSlide() {
     if (currentSlide - 1 >= 0) {
       setCurrentSlide(currentSlide - 1);
     }
-  };
+  }
 
   return (
     <>
@@ -29,31 +33,38 @@ const Slider = ({ slider }) => {
           <AiOutlineLeft />
         </button>
         {slider.map((sliderContent, index) => (
-          <article key={v4()} className={`slider-content ${currentSlide === index ? 'active' : ''}`}>
-            <div className="item-image">
+          <article
+            key={v4()}
+            className={`slider-content ${
+              currentSlide === index ? 'active' : ''
+            }`}>
+            <div className='item-image'>
               <Item
                 original={sliderContent.image.childImageSharp.fluid.src}
                 thumbnail={sliderContent.image.childImageSharp.fluid.src}
-                width="1920"
-                height="1280"
-              >
+                width='1920'
+                height='1280'>
                 {({ ref, open }) => (
                   <div ref={ref} onClick={open}>
-                    <button className="fullscreen">
+                    <button className='fullscreen'>
                       <AiOutlineFullscreen />
                     </button>
-                    <img src={sliderContent.image.childImageSharp.fluid.src} alt={sliderContent.name} title={sliderContent.name} width="600" height="480" />
+                    <img
+                      src={sliderContent.image.childImageSharp.fluid.src}
+                      alt={sliderContent.name}
+                      title={sliderContent.name}
+                      width='600'
+                      height='480'
+                    />
                   </div>
                 )}
               </Item>
             </div>
-            <div className="item-content">
-              <h3 className="has-text-weight-semibold is-size-2">
+            <div className='item-content'>
+              <h3 className='has-text-weight-semibold is-size-2'>
                 {sliderContent.name}
               </h3>
-              <p>
-                {sliderContent.text}
-              </p>
+              <p>{sliderContent.text}</p>
             </div>
           </article>
         ))}
@@ -79,6 +90,6 @@ Slider.propTypes = {
       text: PropTypes.string,
     })
   ),
-}
+};
 
-export default Slider
+export default Slider;
