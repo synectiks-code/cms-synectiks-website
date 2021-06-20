8ad851b53b6403700a4c3521f9a1c2fc36002040
mustafa-synectiks <35029729+mustafa-synectiks@users.noreply.github.com>
2021-06-20 16:43:29 +0530

    Devops Maturity table added to survey form

--- src/templates/survey-page.js
+++ src/templates/survey-page.js
@@ -1,58 +1,56 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { graphql } from 'gatsby'
-import Layout from '../components/Layout'
+import React from 'react';
+import PropTypes from 'prop-types';
+import { graphql } from 'gatsby';
+import Layout from '../components/Layout';
 import SurveyCreator from '../components/Survey';
+import VerticalTabs from '../components/Tabs/VerticalTabs.js';
 
-export const SurveyTemplate = ({
-    Surveyjson
-}) => {
-    const data = Surveyjson ? Surveyjson.data : '[]';
-    return (
-        <section className="section">
-            <div className="container content">
-                <div className="">
-                    <SurveyCreator data={data} />
-                </div>
-            </div>
-        </section>
-    )
-}
+export const SurveyTemplate = ({ Surveyjson }) => {
+  const data = Surveyjson ? Surveyjson.data : '[]';
+  return (
+    <section className='section'>
+      <VerticalTabs />
+      <div className='container content'>
+        <div className=''>
+          <SurveyCreator data={data} />
+        </div>
+      </div>
+    </section>
+  );
+};
 
 SurveyTemplate.propTypes = {
-    Surveyjson: PropTypes.object,
-}
+  Surveyjson: PropTypes.object,
+};
 
 const SurveyPage = ({ data }) => {
-    const { frontmatter } = data.markdownRemark;
+  const { frontmatter } = data.markdownRemark;
 
-    return (
-        <Layout>
-            <SurveyTemplate
-                Surveyjson={frontmatter.Surveyjson}
-            />
-        </Layout>
-    )
-}
+  return (
+    <Layout>
+      <SurveyTemplate Surveyjson={frontmatter.Surveyjson} />
+    </Layout>
+  );
+};
 
 SurveyPage.propTypes = {
-    data: PropTypes.shape({
-        markdownRemark: PropTypes.shape({
-            frontmatter: PropTypes.object,
-        }),
+  data: PropTypes.shape({
+    markdownRemark: PropTypes.shape({
+      frontmatter: PropTypes.object,
     }),
-}
+  }),
+};
 
-export default SurveyPage
+export default SurveyPage;
 
 export const surveyPageQuery = graphql`
   query SurveyPage($id: String!) {
     markdownRemark(id: { eq: $id }) {
       frontmatter {
         Surveyjson {
-            data
+          data
         }
       }
     }
   }
-`
+`;
