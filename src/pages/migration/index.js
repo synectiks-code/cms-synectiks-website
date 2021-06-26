c6bfe84e1a27c841cbab937671f4d99ef911390c
mustafa-synectiks <35029729+mustafa-synectiks@users.noreply.github.com>
2021-06-26 10:08:09 +0530

    Adding slider

--- src/pages/migration/index.js
+++ src/pages/migration/index.js
@@ -40,14 +40,122 @@ const ModernizationWrapper = styled.div`
     }
   }
   .jumbo-bottom {
-    padding: 1em 7em 3em 7em;
+    padding: 1em 1.5em 3em 1.5em;
     color: #212529;
     background-color: #d2d4e0;
     text-align: center;
     margin: 3em 0em;
+    display: grid;
+    grid-gap: 0.4em;
     h3 {
       font-size: 38px;
       margin: 0.2em 0em 1em 0em;
+    }
+    .app {
+      /* display: flex; */
+      display: grid;
+      grid-template-columns: 100px 1fr;
+      color: #fff;
+      align-items: center;
+      grid-gap: 1em;
+      padding: 0.5em 0.5em;
+      background-color: #d62c37 !important;
+      .app-img img {
+        width: 125px;
+        height: 125px;
+        border: none;
+        margin-bottom: -1em;
+        box-shadow: none;
+      }
+      .app-blocks {
+        display: grid;
+        grid-template-columns: repeat(4, 1fr);
+        grid-gap: 1em;
+      }
+      .text-block {
+        height: 13vh;
+        padding: 0.5em 1em;
+        background-color: #fff;
+        color: #212529;
+        p {
+          margin-bottom: 0em;
+        }
+      }
+    }
+    .data {
+      background-color: #f58021 !important;
+      display: flex;
+      /* display: grid; */
+      /* grid-template-columns: repeat(4, 1fr); */
+      color: #fff;
+      align-items: center;
+      grid-gap: 1em;
+      padding: 0.5em 0.5em;
+      .data-img img {
+        width: 185px;
+        height: 125px;
+        border: none;
+        box-shadow: none;
+        margin-bottom: -1em;
+      }
+      .text-block {
+        padding: 0.5em;
+        background-color: #fff;
+        color: #212529;
+        p {
+          margin-bottom: 0em;
+        }
+      }
+    }
+    .process {
+      display: flex;
+      /* display: grid; */
+      /* grid-template-columns: repeat(4, 1fr); */
+      color: #fff;
+      align-items: center;
+      grid-gap: 1em;
+      padding: 0em 0.5em;
+      background-color: #86c542 !important;
+      .pro-img img {
+        width: 130px;
+        height: 125px;
+        border: none;
+        box-shadow: none;
+        margin-bottom: -1em;
+      }
+      .text-block {
+        padding: 0.5em;
+        background-color: #fff;
+        color: #212529;
+        p {
+          margin-bottom: 0em;
+        }
+      }
+    }
+    .capabilities {
+      display: flex;
+      /* display: grid; */
+      /* grid-template-columns: repeat(4, 1fr); */
+      background-color: #6c53a4;
+      color: #fff;
+      align-items: center;
+      grid-gap: 1em;
+      padding: 0em 0.5em;
+    }
+    .infra-img img {
+      width: 125px;
+      height: 125px;
+      border: none;
+      box-shadow: none;
+      margin-bottom: -1em;
+    }
+    .text-block {
+      padding: 1em;
+      background-color: #fff;
+      color: #212529;
+      p {
+        margin-bottom: 0em;
+      }
     }
     img {
       width: 100%;
@@ -116,11 +224,147 @@ const index = () => {
         <div className='jumbo-bottom'>
           {/* <img src={Capabilities} alt='' /> */}
           <h3>Capabilities</h3>
-          <img
+          {/* <img
             src='https://res.cloudinary.com/papu/image/upload/bo_10px_solid_rgb:fff/v1624539278/new-design/home-page/Images/capabilities_eh4vwk.jpg'
             alt=''
-          />
+          /> */}
+
+          <div className='capabilities'>
+            <div className='infra-img'>
+              <img
+                src='https://res.cloudinary.com/papu/image/upload/v1624527145/new-design/home-page/Images/migration_and_modernization_pekzxp.svg'
+                alt=''
+              />
+              <h4>Infra</h4>
+            </div>
+            <div>
+              <h6>Multi-Cloud Hybrid Adoption</h6>
+              <div className='text-block'>
+                <p>Multi cloud Hybrid infra management and operations</p>
+              </div>
+            </div>
+            <div>
+              <h6>Cloud-Migration</h6>
+              <div className='text-block'>
+                <p>Migrate existing workloads to any cloud</p>
+              </div>
+            </div>
+            <div>
+              <h6>Software Defined Transformation</h6>
+              <div className='text-block'>
+                <p>
+                  Make entire infrastructure provisioning software defined and
+                  automated
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className='app'>
+            <div className='app-img'>
+              <img
+                src='https://res.cloudinary.com/papu/image/upload/v1624527145/new-design/home-page/Images/migration_and_modernization_pekzxp.svg'
+                alt=''
+              />
+              <h4>App</h4>
+            </div>
+            <div className='app-blocks'>
+              <div>
+                <h6>Rapid App Development</h6>
+                <div className='text-block'>
+                  <p>
+                    Develop microservice based cloud native custom business App
+                  </p>
+                </div>
+              </div>
+              <div>
+                <h6>App Modernization</h6>
+                <div className='text-block'>
+                  <p>Api / Microservices / Container / DevSecOps Enablement</p>
+                </div>
+              </div>
+              <div>
+                <h6>App Migration</h6>
+                <div className='text-block'>
+                  <p>
+                    Simplify, integrate and deliver applications on any platform
+                  </p>
+                </div>
+              </div>
+              <div>
+                <h6>Cloud Native Transformation</h6>
+                <div className='text-block'>
+                  <p>
+                    Migrate existing outdated proprietary application to Open
+                    cloud native app
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className='data'>
+            <div className='data-img'>
+              <img
+                src='https://res.cloudinary.com/papu/image/upload/v1624527145/new-design/home-page/Images/migration_and_modernization_pekzxp.svg'
+                alt=''
+              />
+              <h4>Data</h4>
+            </div>
+            <div>
+              <h6>Fast-Data Transform</h6>
+              <div className='text-block'>
+                <p>
+                  Increase Data Revenue with Realtime contextual AI-enabled
+                  decision making system
+                </p>
+              </div>
+            </div>
+            <div>
+              <h6>Data Management</h6>
+              <div className='text-block'>
+                <p>
+                  Simplify Data backbone and its lifecycle to a reduced
+                  Capex/Opex and higher business outcome
+                </p>
+              </div>
+            </div>
+            <div>
+              <h6>Data Migration</h6>
+              <div className='text-block'>
+                <p>
+                  Simplify and migrate data from any platform to any platform{' '}
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className='process'>
+            <div className='pro-img'>
+              <img
+                src='https://res.cloudinary.com/papu/image/upload/v1624527145/new-design/home-page/Images/migration_and_modernization_pekzxp.svg'
+                alt=''
+              />
+              <h4>Process</h4>
+            </div>
+            <div>
+              <h6>DevSecOps Enablement</h6>
+              <div className='text-block'>
+                <p>
+                  Improve operational efficiency and development velocity with
+                  DevSecOps adoption
+                </p>
+              </div>
+            </div>
+            <div>
+              <h6>Business Process Automation</h6>
+              <div className='text-block'>
+                <p>
+                  Optimize business processes with observability and data driven
+                  Realtime decision making system
+                </p>
+              </div>
+            </div>
+          </div>
         </div>
+
         <div className='icons'>
           <h3>Differentiator</h3>
           <div className='icon-section'>
